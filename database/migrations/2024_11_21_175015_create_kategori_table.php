c3dbbf34c520982feba755c198567e56dd32ae61
Resyorc <blaskishare@gmail.com>
2024-11-22 09:35:03 +0700

    Update and add new kategori

--- database/migrations/2024_11_21_175015_create_kategori_table.php
+++ database/migrations/2024_11_21_175015_create_kategori_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -15,6 +16,12 @@ return new class extends Migration
             $table->increments('id_kategori');
             $table->string('nama', 100);
         });
+
+        DB::table('kategori')->insert([
+            ['nama' => 'Subsidi'],
+            ['nama' => 'Cluster'],
+            ['nama' => 'Take Over'],
+        ]);
     }
 
     /**
